c7e8929f1690df6d391a0fcbc120f7ff8fec129b
silveroburn <silver0burn@gmail.com>
2025-05-29 17:36:15 +0530

    modified chats

--- lib/chats.dart
+++ lib/chats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'utilities.dart';
 
@@ -7,8 +8,38 @@ class Chats extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.black,
+      drawer: Drawer(
+        backgroundColor: Color.fromARGB(255, 40, 40, 40),
+        child: Padding(
+          padding: EdgeInsets.only(left: 40, top: 30, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  customText('> ', Color.fromARGB(255, 200, 100, 100), 40, 1),
+                  customText('Contacts', Colors.white, 40, 1),
+                  customText('. ', Color.fromARGB(255, 200, 100, 100), 40, 1),
+                ],
+              ),
+              SizedBox(height: 40,),
+              customText('Ashutosh Singh Naruka year 3', Colors.white, 20, 1),
+              SizedBox(height: 20,),
+              customText('Sameery Tiwari', Colors.white, 20, 1),
+              SizedBox(height: 20,),
+              customText('Sanskar', Colors.white, 20, 1),
+              SizedBox(height: 20,),
+              customText('Parth Bansal', Colors.white, 20, 1),
+              SizedBox(height: 20,),
+              customText('Ankit Yadav', Colors.white, 20, 1),
+            ],
+          ),
+        ),
+      ),
       body: Padding(
         padding: EdgeInsets.only(left: 20, right: 20, top: 30),
         child: GestureDetector(
@@ -17,6 +48,11 @@ class Chats extends StatelessWidget{
             showDialog(context: context, builder: (context){
               return longPressMenu(context);
             });
+          },
+          onPanUpdate: (details){
+            if (details.delta.dx > 10){
+              _scaffoldKey.currentState?.openDrawer();
+            }
           },
           child: Column(
             children: [
